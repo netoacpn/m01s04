1d58de5b35e7ad517fe0fdca181948c5d45ba3c8
Antonio Carlos Pereira Neto <133928083+netoacpn@users.noreply.github.com>
2023-06-07 20:45:43 -0300

    Add ex06.js

---
+++ ex06.js
@@ -0,0 +1,4 @@
+window.confirm("A seguir, aparecerão dois campos, solicitando seu nome e sobrenome.")
+var pnome = window.prompt("Qual seu sobre nome?")
+var snome = window.prompt("Qual seu sobre nome?")
+window.alert("Seja bem vindo à nossa página " + pnome + " " + snome + "! Esperamos que goste!")
