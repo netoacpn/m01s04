d5535a538dc26530de7d7f8ac52e5c54d0919667
Antonio Carlos Pereira Neto <133928083+netoacpn@users.noreply.github.com>
2023-06-07 20:16:17 -0300

    First commit

---
+++ ex01.js
@@ -0,0 +1,5 @@
+var n1 = Number(window.prompt ('Digite o valor da nota 1:'))
+var n2 = Number(window.prompt ('Digite o valor da nota 2:'))
+var n3 = Number(window.prompt ('Digite o valor da nota 3:'))
+const media = (n1+n2+n3)/3
+window.alert("Sua média é " + media.toFixed(2))
---
+++ ex02.js
@@ -0,0 +1,4 @@
+var altura = Number(window.prompt ('Digite a altura em metros:'))
+var peso = Number(window.prompt ('Digite o peso em Kg:'))
+const media = peso/(altura**2)
+window.alert("Seu IMC é de" + media.toFixed(2))
