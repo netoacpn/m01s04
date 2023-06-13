3271c731659fa1b8c792e669e39f97ac545a53d8
Antonio Carlos Pereira Neto <133928083+netoacpn@users.noreply.github.com>
2023-06-13 16:55:23 -0300

    Correção da apresentação

--- ex02.js
+++ ex02.js
@@ -1,4 +1,4 @@
 var altura = Number(window.prompt ('Digite a altura em metros:'))
 var peso = Number(window.prompt ('Digite o peso em Kg:'))
 const media = peso/(altura**2)
-window.alert("Seu IMC é de" + media.toFixed(2))
+window.alert("Seu IMC é de " + media.toFixed(2))
--- ex06.js
+++ ex06.js
@@ -1,4 +1,4 @@
 window.confirm("A seguir, aparecerão dois campos, solicitando seu nome e sobrenome.")
-var pnome = window.prompt("Qual seu sobre nome?")
-var snome = window.prompt("Qual seu sobre nome?")
+var pnome = String(window.prompt("Qual seu nome?"))
+var snome = String(window.prompt("Qual seu sobre nome?"))
 window.alert("Seja bem vindo à nossa página " + pnome + " " + snome + "! Esperamos que goste!")
